37b1e87eaf64a67b405cd6cbc58276d01365a16b
AdongoNat <danieladongonat@gmail.com>
2025-10-07 23:43:26 +0000

    new

--- src/app/api/seller/[id]/products/route.ts
+++ src/app/api/seller/[id]/products/route.ts
@@ -23,8 +23,6 @@ export async function GET(
 
 
 
-
-    
     return NextResponse.json({ products }, { status: 200 });
   } catch (error) {
     console.error("Error fetching seller products:", error);
