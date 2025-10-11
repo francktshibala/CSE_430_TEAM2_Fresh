7afc804eddc2abd250d0c7ab9e51c1beee1e56e1
lishdiane <dlish@byupathway.edu>
2025-10-10 19:38:46 -0600

    fixed new product loading

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,14 +1,15 @@
+export const dynamic = "force-dynamic";
+
 import styles from "@/app/page.module.css";
 import { getAllCategories, getAllProducts, getUserById } from "@/lib/data";
 import { Product } from "@/lib/interfaces";
 import { calculateAverage } from "@/lib/utils";
-import ProductList from "@/app/ui/products/product-list"
+import ProductList from "@/app/ui/products/product-list";
 
 export default async function ProductPage() {
   const products: Product[] = await getAllProducts();
   const categories = await getAllCategories();
 
-  
   const productsWithRatings = await Promise.all(
     products.map(async (product) => {
       console.log(product);
@@ -18,7 +19,7 @@ export default async function ProductPage() {
       return {
         ...product,
         average,
-        user
+        user,
       };
     })
   );
@@ -27,11 +28,7 @@ export default async function ProductPage() {
     <section className={styles.featuredSection}>
       <h2>Explore Our Products</h2>
       <p>From artisan hands to your home</p>
-      <ProductList
-        products={productsWithRatings}
-        categories={categories}
-      />
+      <ProductList products={productsWithRatings} categories={categories} />
     </section>
   );
 }
-
